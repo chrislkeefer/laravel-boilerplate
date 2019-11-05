dbacf3d846e75a373a3d2b8194939b901f61810e
Chris Keefer <42353349+chrislkeefer@users.noreply.github.com>
2019-11-05 10:10:57 -0800

    introduce interactions to base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Run an interaction method.
+     *
+     * @param  string  $interaction
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function interact($interaction, array $parameters = [])
+    {
+        if (! Str::contains($interaction, '@')) {
+            $interaction = $interaction.'@handle';
+        }
+
+        list($class, $method) = explode('@', $interaction);
+
+        return call_user_func_array([app($class), $method], $parameters);
+    }
+
+    /**
+     * Run an interaction method.
+     *
+     * @param  string  $interaction
+     * @param  array  $parameters
+     * @return mixed
+     */
+    public function call($interaction, array $parameters = [])
+    {
+        return $this->interact($interaction, $parameters);
+    }
 }
